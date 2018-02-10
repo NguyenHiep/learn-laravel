0c0e433dc323691d3df203b6bdf0fafa0365c36a
Nguyen Hiep <minhhiep.q@gmail.com>
2018-02-10 11:43:46 +0700

    [update] update styles for app

--- resources/views/manage/blocks/header.blade.php
+++ resources/views/manage/blocks/header.blade.php
@@ -21,39 +21,9 @@
   <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet"
           type="text/css"/>
-    <link href="{{asset('/manages/assets/global/plugins/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet"
-          type="text/css"/>
-    <link href="{{asset('/manages/assets/global/plugins/simple-line-icons/simple-line-icons.min.css')}}"
-          rel="stylesheet" type="text/css"/>
-    <link href="{{asset('/manages/assets/global/plugins/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet"
-          type="text/css"/>
-    <link href="{{asset('/manages/assets/global/plugins/uniform/css/uniform.default.css')}}" rel="stylesheet"
-          type="text/css"/>
-    <link href="{{asset('/manages/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css')}}"
-          rel="stylesheet" type="text/css"/>
-    <!-- END GLOBAL MANDATORY STYLES -->
-    <!-- BEGIN PAGE LEVEL PLUGINS -->
-    <link href="{{asset('/manages/assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.css')}}"
-          rel="stylesheet" type="text/css"/>
-    <link href="{{asset('/manages/assets/global/plugins/morris/morris.css')}}" rel="stylesheet" type="text/css"/>
-    <link href="{{asset('/manages/assets/global/plugins/fullcalendar/fullcalendar.min.css')}}" rel="stylesheet"
-          type="text/css"/>
-    <link href="{{asset('/manages/assets/global/plugins/jqvmap/jqvmap/jqvmap.css')}}" rel="stylesheet" type="text/css"/>
-    <link href="{{asset('/manages/assets/global/plugins/bootstrap-modal/css/bootstrap-modal-bs3patch.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('/manages/assets/global/plugins/bootstrap-modal/css/bootstrap-modal.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('/manages/assets/global/plugins/dropzone/dropzone.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('/manages/assets/global/plugins/dropzone/basic.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('/manages/assets/global/plugins/bootstrap-toastr/toastr.min.css') }}" rel="stylesheet" type="text/css" />
-    <!-- END PAGE LEVEL PLUGINS -->
-  <!-- BEGIN THEME GLOBAL STYLES -->
-    <link href="{{asset('/manages/assets/global/css/components.min.css')}}" rel="stylesheet" id="style_components"
-          type="text/css"/>
-    <link href="{{asset('/manages/assets/global/css/plugins.min.css')}}" rel="stylesheet" type="text/css"/>
-    <!-- END THEME GLOBAL STYLES -->
-    <!-- BEGIN THEME LAYOUT STYLES -->
-    <link href="{{asset('/manages/assets/layouts/layout/css/layout.min.css')}}" rel="stylesheet" type="text/css"/>
-    <link href="{{asset('/manages/assets/layouts/layout/css/themes/darkblue.min.css')}}" rel="stylesheet"
-          type="text/css" id="style_color"/>
+    <link rel="stylesheet" href="{{ asset('/manages/assets/global/plugins/font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('manages/assets/global/plugins/simple-line-icons/simple-line-icons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     <link href="{{asset('/manages/assets/layouts/layout/css/custom.min.css?v='.VERSION)}}" rel="stylesheet" type="text/css"/>
 
     <!-- END THEME LAYOUT STYLES -->
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,22 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
   .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.styles([
+  'public/manages/assets/global/plugins/bootstrap/css/bootstrap.min.css',
+  'public/manages/assets/global/plugins/uniform/css/uniform.default.css',
+  'public/manages/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css',
+  'public/manages/assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.css',
+  'public/manages/assets/global/plugins/morris/morris.css',
+  'public/manages/assets/global/plugins/fullcalendar/fullcalendar.min.css',
+  'public/manages/assets/global/plugins/jqvmap/jqvmap/jqvmap.cs',
+  'public/manages/assets/global/plugins/bootstrap-modal/css/bootstrap-modal-bs3patch.css',
+  'public/manages/assets/global/plugins/bootstrap-modal/css/bootstrap-modal.cs',
+  'public/manages/assets/global/plugins/dropzone/dropzone.min.css',
+  'public/manages/assets/global/plugins/dropzone/basic.min.css',
+  'public/manages/assets/global/plugins/bootstrap-toastr/toastr.min.css',
+  'public/manages/assets/global/css/components.min.css',
+  'public/manages/assets/global/css/plugins.min.css',
+  'public/manages/assets/layouts/layout/css/layout.min.css',
+  'public/manages/assets/layouts/layout/css/themes/darkblue.min.css',
+], 'public/css/app.css');
