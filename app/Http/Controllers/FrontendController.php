61fc4c150b47d752b0edc4dee7f6d1cc14de89cf
Nguyen Hiep <minhhiep.q@gmail.com>
2018-02-27 07:22:38 +0700

    [change] update product page home

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -2,7 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Products;
+
 class FrontendController extends Controller
 {
 
+    protected function getBestsellerProducts(int $limit = 8)
+    {
+
+    }
+
+    protected function getNewProducts(int $limit = 8)
+    {
+
+    }
+
+    protected function getProductByCategoryId(int $cat_id, int $limit = 8)
+    {
+        $products = Products::where('category_id', 'like', '%|' . $cat_id . '|%')
+            ->orderBy('id')
+            ->limit($limit)
+            ->get();
+        return $products;
+    }
+
+    protected function getRelatedProducts(int $limit = 8)
+    {
+
+    }
+
+    protected function getRandomProducts(int $limit = 8)
+    {
+        $product = Products::where('status', STATUS_ENABLE)->inRandomOrder()->limit($limit)->get();
+        return $product;
+    }
+
+    protected function getSaleProducts(int $limit = 8)
+    {
+
+    }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,8 +7,21 @@ use Illuminate\Http\Request;
 
 class HomeController extends FrontendController
 {
+    const THOITRANG_NAM     = 2;
+    const THOITRANG_NU      = 3;
+    const THOITRANG_CHOBE   = 4;
+    const PHUKIEN_THOITRANG = 5;
+
     public function index(){
-        $data['sliders'] = Sliders::where('slider_status', STATUS_ENABLE)->get();
+
+        $data['sliders']            = Sliders::where('slider_status', STATUS_ENABLE)->get();
+        $data['products_random']    = $this->getRandomProducts();
+        $data['thoitrang_nu']       = $this->getProductByCategoryId(static::THOITRANG_NU);
+        $data['thoitrang_nam']      = $this->getProductByCategoryId(static::THOITRANG_NAM);
+        $data['thoitrang_chobe']    = $this->getProductByCategoryId(static::THOITRANG_CHOBE);
+        $data['phukien_thoitrang']  = $this->getProductByCategoryId(static::PHUKIEN_THOITRANG);
+
     	return view('frontend.theme-ecommerce.home', $data);
     }
+
 }
