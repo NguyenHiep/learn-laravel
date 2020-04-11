2efa17718f87daa73ef4aa5d7d0f102d8c192f53
Hiep Nguyen <hiep.nguyen@hdwebsoft.co>
2020-04-12 01:32:27 +0700

    [change] Permission for menu sidebar

--- resources/views/manage/blocks/sidebar.blade.php
+++ resources/views/manage/blocks/sidebar.blade.php
@@ -1,4 +1,5 @@
 <!-- BEGIN SIDEBAR -->
+@if(Auth::guard('user')->check())
 <div class="page-sidebar-wrapper">
   <div class="page-sidebar navbar-collapse collapse">
     <ul class="page-sidebar-menu  page-header-fixed page-sidebar-menu-light " data-keep-expanded="false"
@@ -8,6 +9,7 @@
         <div class="sidebar-toggler"></div>
         <!-- END SIDEBAR TOGGLER BUTTON -->
       </li>
+      @can('dashboard-list')
       <li class="nav-item start {{ active(['manage.dashboard'],'active open') }} ">
         <a href="{{ route('manage.dashboard') }}" class="nav-link nav-toggle">
           <i class="icon-home"></i>
@@ -15,13 +17,12 @@
           <span class="selected"></span>
         </a>
       </li>
-      <li class="nav-item  {{ active(['manage.categories.index', 'manage.categories.create', 'manage.categories.edit'], 'active open') }}">
-        <a href="{{ route('manage.categories.index') }}" class="nav-link ">
-          <i class="fa fa-tasks"></i>
-          <span class="title">{{__('static.sidebars.manage.categories.title')}}</span>
-        </a>
-      </li>
-      <li class="nav-item  {{ active(['manage.products.index', 'manage.products.create', 'manage.products.edit'], 'active open') }}">
+      @endcan
+      @canany(['product-list', 'product-create', 'category-list'])
+      <li class="nav-item  {{ active([
+    'manage.products.index', 'manage.products.create', 'manage.products.edit',
+    'manage.categories.index', 'manage.categories.create', 'manage.categories.edit'
+    ], 'active open') }}">
         <a href="{{ route('manage.products.index') }}" class="nav-link nav-toggle">
           <i class="icon-graph"></i>
           <span class="title">{{__('static.sidebars.manage.products.title')}}</span>
@@ -29,25 +30,31 @@
           <span class="selected"></span>
         </a>
         <ul class="sub-menu">
+          @can('product-list')
           <li class="nav-item">
             <a href="{{ route('manage.products.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.products.list')}}</span>
             </a>
           </li>
-          
+          @endcan
+          @can('product-create')
           <li class="nav-item">
             <a href="{{ route('manage.products.create') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.products.create')}}</span>
             </a>
           </li>
+          @endcan
+          @can('category-list')
           <li class="nav-item">
             <a href="{{ route('manage.categories.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.categories.title')}}</span>
             </a>
           </li>
-
+          @endcan
         </ul>
       </li>
+      @endcanany
+      @canany(['order-list', 'order-create'])
       <li
         class="nav-item  {{ active(['manage.orders.index', 'manage.orders.create', 'manage.orders.edit', 'manage.orders.invoice', 'manage.orders.show'], 'active open') }}">
         <a href="{{ route('manage.orders.index') }}" class="nav-link nav-toggle">
@@ -55,69 +62,88 @@
           <span class="title">{{__('static.sidebars.manage.orders.title')}}</span>
         </a>
         <ul class="sub-menu">
-
+          @can('order-list')
           <li class="nav-item">
             <a href="{{ route('manage.orders.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.orders.list')}}</span>
             </a>
           </li>
+          @endcan
+          @can('order-create')
           <li class="nav-item">
             <a href="{{ route('manage.orders.create') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.orders.create')}}</span>
             </a>
           </li>
-
+          @endcan
         </ul>
       </li>
-      <li class="nav-item  {{ active(['manage.posts.index', 'manage.posts.create', 'manage.posts.edit', 'manage.category.index', 'manage.category.create','manage.category.edit', 'manage.tags.index', 'manage.tags.create', 'manage.tags.edit'], 'active open') }}">
+      @endcanany
+      @canany(['post-list', 'post-create', 'post-category-list', 'post-tag-list'])
+      <li class="nav-item {{ active(['manage.posts.index', 'manage.posts.create', 'manage.posts.edit', 'manage.category.index', 'manage.category.create','manage.category.edit', 'manage.tags.index', 'manage.tags.create', 'manage.tags.edit'], 'active open') }}">
         <a href="{{ route('manage.posts.index') }}" class="nav-link nav-toggle">
           <i class="icon-pin"></i>
           <span class="title">{{__('static.sidebars.manage.posts.title')}}</span>
           <span class="arrow {{ active(['manage.posts.index'], 'open') }}"></span>
         </a>
         <ul class="sub-menu">
-          <li class="nav-item  ">
+          @can('post-list')
+          <li class="nav-item">
             <a href="{{ route('manage.posts.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.posts.posts')}}</span>
             </a>
           </li>
-          <li class="nav-item  ">
+          @endcan
+          @can('post-create')
+          <li class="nav-item">
             <a href="{{ route('manage.posts.create') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.posts.creates')}}</span>
             </a>
           </li>
-          <li class="nav-item  ">
+          @endcan
+          @can('post-category-list')
+          <li class="nav-item">
             <a href="{{ route('manage.category.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.posts.category')}}</span>
             </a>
           </li>
+          @endcan
+          @can('post-tag-list')
           <li class="nav-item  ">
             <a href="{{ route('manage.tags.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.posts.tags')}}</span>
             </a>
           </li>
-
+          @endcan
         </ul>
       </li>
-      <li class="nav-item  {{ active(['manage.medias.index', 'manage.medias.create', 'manage.medias.edit'], 'active open') }}">
+      @endcanany
+      @canany(['media-list', 'media-create'])
+      <li class="nav-item {{ active(['manage.medias.index', 'manage.medias.create', 'manage.medias.edit'], 'active open') }}">
         <a href="javascript:;" class="nav-link nav-toggle">
           <i class="icon-picture"></i>
           <span class="title">{{__('static.sidebars.manage.medias.title')}}</span>
           <span class="arrow {{ active(['manage.medias.index', 'manage.medias.create', 'manage.medias.edit'], 'open') }}"></span>
         </a>
         <ul class="sub-menu">
-          <li class="nav-item  ">
+          @can('media-list')
+          <li class="nav-item">
             <a href="{{ route('manage.medias.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.medias.medias')}}</span>
             </a>
           </li>
-          <li class="nav-item  ">
+          @endcan
+          @can('media-create')
+          <li class="nav-item">
             <a href="{{ route('manage.medias.create') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.medias.creates')}}</span>
             </a>
           </li>
+          @endcan
         </ul>
       </li>
+      @endcanany
+      @canany(['slider-list', 'slider-create'])
       <li class="nav-item  {{ active(['manage.sliders.index', 'manage.sliders.create', 'manage.sliders.edit'], 'active open') }}">
         <a href="javascript:;" class="nav-link nav-toggle">
           <i class="icon-picture"></i>
@@ -125,43 +151,57 @@
           <span class="arrow {{ active(['manage.sliders.index', 'manage.sliders.create', 'manage.sliders.edit'], 'open') }}"></span>
         </a>
         <ul class="sub-menu">
-          <li class="nav-item  ">
+          @can('slider-list')
+          <li class="nav-item">
             <a href="{{ route('manage.sliders.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.sliders.sliders')}}</span>
             </a>
           </li>
-          <li class="nav-item  ">
+          @endcan
+          @can('slider-create')
+          <li class="nav-item">
             <a href="{{ route('manage.sliders.create') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.sliders.creates')}}</span>
             </a>
           </li>
+          @endcan
         </ul>
       </li>
-      <li class="nav-item  {{ active(['manage.pages.index', 'manage.pages.create', 'manage.pages.edit'], 'active open') }}">
+      @endcanany
+      @canany(['page-list', 'page-create'])
+      <li class="nav-item {{ active(['manage.pages.index', 'manage.pages.create', 'manage.pages.edit'], 'active open') }}">
         <a href="{{ route('manage.pages.index') }}" class="nav-link nav-toggle">
           <i class="icon-docs"></i>
           <span class="title">{{__('static.sidebars.manage.pages.title')}}</span>
           <span class="arrow {{ active(['manage.pages.index', 'manage.pages.create', 'manage.pages.edit'], 'open') }}"></span>
         </a>
         <ul class="sub-menu">
-          <li class="nav-item  ">
+          @can('page-list')
+          <li class="nav-item">
             <a href="{{ route('manage.pages.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.pages.pages')}}</span>
             </a>
           </li>
-          <li class="nav-item  ">
+          @endcan
+          @can('page-create')
+          <li class="nav-item">
             <a href="{{ route('manage.pages.create') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.pages.creates')}}</span>
             </a>
           </li>
+          @endcan
         </ul>
       </li>
+      @endcanany
+      @can('comment-list')
       <li class="nav-item">
         <a href="{{ route('manage.comments.index') }}" class="nav-link nav-toggle">
           <i class="icon-bubbles"></i>
           <span class="title">{{__('static.sidebars.manage.comments')}}</span>
         </a>
       </li>
+      @endcan
+      @canany(['customer-list', 'user-list', 'role-list'])
       <li class="nav-item {{ active([
     'manage.customers.index', 'manage.customers.create', 'manage.customers.edit',
     'manage.admins.index', 'manage.admins.create', 'manage.admins.edit',
@@ -179,23 +219,31 @@
           <span class="selected"></span>
         </a>
         <ul class="sub-menu">
+          @can('customer-list')
           <li class="nav-item">
             <a href="{{ route('manage.customers.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.customers.customers')}}</span>
             </a>
           </li>
+          @endcan
+          @can('user-list')
           <li class="nav-item">
             <a href="{{ route('manage.admins.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.customers.admins')}}</span>
             </a>
           </li>
+          @endcan
+          @can('role-list')
           <li class="nav-item  ">
             <a href="{{ route('manage.roles.index') }}" class="nav-link ">
               <span class="title">{{__('static.sidebars.manage.customers.roles')}}</span>
             </a>
           </li>
+          @endcan
         </ul>
       </li>
+      @endcanany
+      @can('email-list')
       <li class="nav-item {{ active(['manage.email.index'], 'active open') }}">
         <a href="{{ route('manage.email.index') }}" class="nav-link nav-toggle">
           <i class="icon-envelope"></i>
@@ -216,6 +264,8 @@
           </li>
         </ul>
       </li>
+      @endcan
+      @can('setting-list')
       <li
         class="nav-item  {{ active(['manage.settings.index'], 'active open') }}">
         <a href="{{ route('manage.settings.index') }}" class="nav-link nav-toggle">
@@ -232,7 +282,9 @@
           </li>
         </ul>
       </li>
+      @endcan
     </ul>
   </div>
 </div>
+@endif
 <!-- END SIDEBAR -->
